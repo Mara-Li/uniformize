c2a8a37c6257f68cead56b18b7c3bb6e72525422
Mara <mara-li@outlook.fr>
2024-11-02 09:40:47 +0100

    test: add more test for capitalize & toTitle

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,16 +2,31 @@ import {describe, expect, test} from "bun:test";
 import "src";
 
 describe("standardize", () => {
-	test('simple', () => {
+	test('Héllô Wôrld → hello world', () => {
 		expect("Héllô Wôrld".standardize()).toBe("hello world");
 	});
 });
 
+describe("capitalize", () => {
+	test("été ⇒ Été", () =>{
+		expect("été".capitalize()).toBe("Été");
+	});
+	test("hello world → Hello World", () => {
+		expect("hello world".capitalize()).toBe("Hello World");
+	});
+});
+
+describe('toTitle', () => {
+	test('hello world → Hello world', () => {
+		expect("hello world".toTitle()).toBe("Hello world");
+	});
+});
+
 describe("Subtext", () =>{
-	test("simple subtext", () =>{
+	test("hello world ⇒ (Hello) ⇒ True", () =>{
 		expect("Hello World".subText("Hello")).toBe(true);
 	});
-	test("with number", () =>{
+	test("discord channel subtext", () =>{
 		expect("-# ↪ https://discord.com/channels/1295390060828616961/1295390188229652837/1295390616847472181".subText("-# ↪ https://discord.com/channels/1295390060828616961")).toBe(true);
 	});
 	test("empty query", () =>{
@@ -19,5 +34,14 @@ describe("Subtext", () =>{
 	});
 	test("null query", () =>{
 		expect("Hello World".subText(null)).toBe(false);
+	});
+	test("null ⇒ false", () =>{
+		expect("hello".subText(null)).toBe(false);
+	});
+	test("undef ⇒ false", () =>{
+		expect("hello".subText(undefined)).toBe(false);
+	});
+	test("empty ⇒ true", () =>{
+		expect("hello".subText("")).toBe(true);
 	});
 })
